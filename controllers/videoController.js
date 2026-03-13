1623c2271426df3439db58c3669fb1a8ad629960
a7medmo7amady <ahmedmohamady2005@gmail.com>
2026-03-13 20:22:41 +0200

    part3 is done

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -15,3 +15,13 @@ export const createVideo = async (req, res) => {
         res.status(500).json({ message: err.message });
     }
 };
+export const getVideos = async (req, res) => {
+    try {
+        const videos = await Video.find()
+            .populate("user", "name email"); 
+
+        res.status(200).json(videos);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
