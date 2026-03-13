6cc949fa423261b8cfbed484ce2f1b8387032b34
5ose <s-youssef.kamel@zewailcity.edu.eg>
2026-03-13 21:02:26 +0200

    Undeded changes

---
+++ models/videoModel.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const videoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    url: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      default: "General",
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Video", videoSchema);
